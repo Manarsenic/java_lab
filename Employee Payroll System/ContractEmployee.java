def215b23d5f6506dd3397e7bd56418411ef7d5a
Manas Kumar <188164306+Manarsenic@users.noreply.github.com>
2026-04-09 13:00:35 +0530

    Add ContractEmployee class for hourly-based employees

---
+++ Employee Payroll System/ContractEmployee.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+public class ContractEmployee extends Employee {
+    private int noOfHours;
+    private double hourlyRate;
+
+    public ContractEmployee(String name, String panNo, Date joiningDate,
+                            String designation, int empId,
+                            int noOfHours, double hourlyRate)
+                            throws InvalidSalaryException {
+
+        super(name, panNo, joiningDate, designation, empId);
+
+        if (noOfHours < 0 || hourlyRate < 0) {
+            throw new InvalidSalaryException("Hours or hourly rate cannot be negative!");
+        }
+
+        this.noOfHours = noOfHours;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calcCTC() {
+        return noOfHours * hourlyRate;
+    }
+}
